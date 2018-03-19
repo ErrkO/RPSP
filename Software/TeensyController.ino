8ff6b656e4551e6df95780c018e00db6f4e4cc10
Eric Oliver <eoliver4@my.apsu.edu>
2018-03-19 14:18:19 -0500

    Drivers
    Started working on the drivers for the RPSP

---
+++ Software/TeensyController.ino
@@ -0,0 +1,30 @@
+
+/*********************
+*   Pin Definitions  *
+*********************/
+
+// Directional pins
+const int pinBtnUp = 1;
+const int pinBtnDwn = 2;
+const int pinBtnRght = 3;
+const int pinBtnLft = 4;
+
+// Start/Select pins
+const int pinBtnStrt = 5;
+const int pinBtnSlct = 6;
+
+// Face Button pins
+const int pinBtnA = 7;
+const int pinBtnB = 8;
+const int pinBtnX = 9;
+const int pinBtnY = 10;
+
+// Shoulder pins
+const int pinBtnLftShldr = 11;
+const int pinBtnLftTrggr = 12;
+const int pinBtnRghtShldr = 13;
+const int pinBtnRghtTrggr = 14;
+
+// Anolgue sticks
+
+
